26fdd7976920196431b9279b3984d78bb30f4496
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-02-06 18:48:49 +0200

    added welcome component

--- src/pages/welcome-page/welcomePage.js
+++ src/pages/welcome-page/welcomePage.js
@@ -1,6 +1,16 @@
 import React from "react";
-import "./welcomePage.scss"
+import "./welcomePage.scss";
+import Login from "../../components/login/Login";
 
 export default function welcomePage() {
-  return <div>welcome page</div>;
+  return (
+    <div className="welcomePage-container">
+      <h2 className="welcomePage-header">Welcom to Vaad-Bayit</h2>
+      <p>
+        Our mission is to help you manage your residents, <br /> financial
+        reports and other tasks.
+      </p>
+      <Login />
+    </div>
+  );
 }
