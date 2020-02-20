7d4ce0db3a160bedb4fd43a349820d266b1af56a
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-02-20 14:47:27 +0200

    fixes and changes in column names

--- server/initialize_db.sql
+++ server/initialize_db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    user_uid UUID PRIMARY KEY,
+    user_id UUID PRIMARY KEY,
     first_name text NOT NULL,
     last_name text NOT NULL,
     email text UNIQUE,
@@ -10,44 +10,44 @@ CREATE TABLE users
 
 CREATE TABLE buildings
 (
-    building_uid UUID PRIMARY KEY,
+    building_id UUID PRIMARY KEY,
     building_created timestamp NOT NULL,
     city text NOT NULL,
     street text NOT NULL,
-    building_num integer NOT NULL
+    building_number integer NOT NULL
 );
 
 CREATE TABLE apartments
 (
-    apartment_uid uuid PRIMARY KEY,
+    apartment_id uuid PRIMARY KEY,
     owner_name text NOT NULL,
     renter_name text,
     apartment_number integer UNIQUE,
     floor_number integer,
     is_rented BOOLEAN NOT NULL,
     apartment_created timestamp NOT NULL,
-    building_uid UUID NOT NULL,
-    FOREIGN KEY (building_uid) REFERENCES buildings(building_uid)
+    building_id UUID NOT NULL,
+    FOREIGN KEY (building_id) REFERENCES buildings(building_id)
 );
 
 CREATE TABLE buildings_users
 (
-    building_uid UUID NOT NULL,
-    user_uid UUID NOT NULL,
+    building_id UUID NOT NULL,
+    user_id UUID NOT NULL,
     bu_created timestamp NOT NULL,
-    PRIMARY KEY (building_uid, user_uid),
-    FOREIGN KEY (building_uid) REFERENCES buildings(building_uid) ON UPDATE CASCADE,
-    FOREIGN KEY (user_uid) REFERENCES users(user_uid) ON UPDATE CASCADE
+    PRIMARY KEY (building_id, user_id),
+    FOREIGN KEY (building_id) REFERENCES buildings(building_id) ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE
 );
 
 insert into users
-    (user_uid, first_name, last_name, email, password, user_created)
+    (user_id, first_name, last_name, email, password, user_created)
 VALUES
     (uuid_generate_v4(), 'tomer', 'matmon', 'tomer.matmon@gmail.com', crypt('tomermatmon', gen_salt('bf', 4)), now()),
     (uuid_generate_v4(), 'avner', 'aricha', 'avner.aricha@gmail.com', crypt('avneraricha', gen_salt('bf', 4)), now());
 
 insert into buildings
-    (building_uid, building_created, city, street, building_num)
+    (building_id, building_created, city, street, building_number)
 VALUES
     (uuid_generate_v4(), now(), 'haifa', 'eliezer alter', 3),
     (uuid_generate_v4(), now(), 'haifa', 'avraham sachnin', 4);
