3445491185d41b1efcb5ff83d0fe725fa315fd9b
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-02-20 11:12:17 +0200

    minor changes to DB initializer

--- server/initialize_db.sql
+++ server/initialize_db.sql
@@ -5,7 +5,7 @@ CREATE TABLE users
     last_name text NOT NULL,
     email text UNIQUE,
     password text,
-    user_created timestamp NOT NULL,
+    user_created timestamp NOT NULL
 );
 
 CREATE TABLE buildings
@@ -14,7 +14,7 @@ CREATE TABLE buildings
     building_created timestamp NOT NULL,
     city text NOT NULL,
     street text NOT NULL,
-    num integer NOT NULL
+    building_num integer NOT NULL
 );
 
 CREATE TABLE apartments
@@ -24,11 +24,11 @@ CREATE TABLE apartments
     renter_name text,
     apartment_number integer UNIQUE,
     floor_number integer,
-    is_rented BOOLEAN,
-    user_created timestamp NOT NULL,
+    is_rented BOOLEAN NOT NULL,
+    apartment_created timestamp NOT NULL,
     building_uid UUID NOT NULL,
     FOREIGN KEY (building_uid) REFERENCES buildings(building_uid)
-)
+);
 
 CREATE TABLE buildings_users
 (
@@ -41,12 +41,13 @@ CREATE TABLE buildings_users
 );
 
 insert into users
-    (user_uid, first_name, last_name, email, password, user_created, apartment_number, floor_number, is_owner)
+    (user_uid, first_name, last_name, email, password, user_created)
 VALUES
-    (uuid_generate_v4(), 'tomer', 'matmon', 'tomer.matmon@gmail.com', crypt('tomermatmon', gen_salt('bf', 4)), now(), 6, 3, true),
-    (uuid_generate_v4(), 'sharon', 'aricha matmon', 'sharon.aricha.matmon@gmail.com', crypt('sharonarichamatmon', gen_salt('bf', 4)), now(), 6, 3, true);
+    (uuid_generate_v4(), 'tomer', 'matmon', 'tomer.matmon@gmail.com', crypt('tomermatmon', gen_salt('bf', 4)), now()),
+    (uuid_generate_v4(), 'avner', 'aricha', 'avner.aricha@gmail.com', crypt('avneraricha', gen_salt('bf', 4)), now());
 
 insert into buildings
-    (building_uid, building_created, city, street, num)
+    (building_uid, building_created, city, street, building_num)
 VALUES
-    (uuid_generate_v4(), now(), 'haifa', 'alter', 3);
+    (uuid_generate_v4(), now(), 'haifa', 'eliezer alter', 3),
+    (uuid_generate_v4(), now(), 'haifa', 'avraham sachnin', 4);
