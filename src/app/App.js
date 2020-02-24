266fa3953e3f88b0d87360ddfcc009a4e7583af6
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-02-24 10:39:49 +0200

    removed redirect path to unused component

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./app.scss";
-import { Router, Switch, Route, Redirect } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
 import history from "../history";
 
 import Nav from "../components/nav/Nav";
-import ResidentForm from "../components/residentForm/ResidentForm";
 import HomePage from "../pages/home-page/HomePage";
 import ResidentsPage from "../pages/residents-page/ResidentsPage";
 import AddResidentPage from "../pages/addResident-page/AddResidentPage";
