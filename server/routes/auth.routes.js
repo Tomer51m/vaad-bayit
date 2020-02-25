cf686f45912f69d15a463c967c56ff848b82204b
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-02-25 16:10:09 +0200

    updated auth route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -15,68 +15,82 @@ const config = {
 };
 const pool = new Pool(config);
 
-// create new user // + create building for user //
+// create new user + create building for user + assign building to user//
 route.post("/api/auth/signup", async (req, res) => {
   try {
+    console.log("request body", req.body)
     await createUserSchema.validate(req.body, { abortEarly: false }); // need to refactor as middleware //
 
     const {
       firstName: first_name,
       lastName: last_name,
       email,
-      password
+      password,
+      city,
+      street,
+      number
     } = req.body;
 
-    const uuid = uuidv4();
+    const user_uuid = uuidv4();
     const user_created = "2020-02-20 11:11:11.554346"; // need to fix date //
     const passwordHash = await bcrypt.hash(password, 4);
-    const queryTemplate = "INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6)";
-    await pool.query(queryTemplate, [
-      uuid,
+    const querynewUser = "INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6)";
+    await pool.query(querynewUser, [
+      user_uuid,
       first_name,
       last_name,
       email,
       passwordHash,
       user_created
     ]);
+    
+    const building_uuid = uuidv4();
+    const building_created = "2020-02-20 11:11:11.554346"; // need to fix date //
+    const queryNewBuilding = "INSERT INTO buildings VALUES ($1, $2, $3, $4, $5)";
+    await pool.query(queryNewBuilding, [building_uuid, building_created, city, street, number]);
+    
+    const bu_created = "2020-02-20 11:11:11.554346"; // need to fix date //
+    const queryNewBU = "INSERT INTO buildings_users VALUES ($1, $2, $3)";
+    await pool.query(queryNewBU, [building_uuid, user_uuid, bu_created]);
 
     const querycreatedUser = "SELECT * FROM users WHERE user_id = $1";
-    const response = await pool.query(querycreatedUser, [uuid]);
+    const newUser = await pool.query(querycreatedUser, [user_uuid]);
 
-    res.json(response.rows);
+    res.json(newUser.rows[0]);
   } catch (err) {
-    res.status(500).json({
-      message: "cannot create user",
-      error: err.stack
-    });
     console.error(err);
+    res.status(500).json({
+      message: "signup failed",
+      error: "can't signup user"
+    });
   }
 });
 
 // login user //
-route.get("/api/auth/login", async (req, res) => {
+route.post("/api/auth/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const queryTemplate = "SELECT * FROM users WHERE email = $1";
     const response = await pool.query(queryTemplate, [email]);
-    console.log("response", response);
     const user = response.rows[0];
-    console.log("user", user);
     const validPassword = await bcrypt.compare(password, user.password);
 
     if (validPassword) {
       const Token = jwt.sign({ uid: user.user_id }, process.env.TOKEN_SECRET);
       res.header("Authorization", `Bearer ${Token}`).json(user);
     } else {
-      res.json("password incorrect");
+      res.status(401).json({
+        message: "Login failed",
+        error: "Email or password incorrect"
+      });
     }
   } catch (err) {
+    console.error(err);
     res.status(500).json({
-      message: "cannot login user",
-      error: err.stack
+      message: "Login failed",
+      error: "Email or password incorrect"
     });
-    console.log(err.stack);
   }
 });
 
