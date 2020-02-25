cc149bb724dac02c213b136c42cc04dad7182c3f
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-02-25 16:13:12 +0200

    add user name to nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./nav.scss";
 import { Link, withRouter } from "react-router-dom";
-import { setAuthentication } from "../../Auth";
 import history from "../../history";
+import { useSelector } from "react-redux";
 
 function Nav({ match }) {
+  const { user } = useSelector(state => state.users);
+  console.log("nav user", user);
   return (
     <div className="header-container">
       <nav className="nav" role="navigation">
@@ -17,10 +19,10 @@ function Nav({ match }) {
         <li className="list-item">
           <Link to={`${match.url}/add`}>Add Resident</Link>
         </li>
+        <span className="user-greeting">Hello {user.first_name} {user.last_name}</span>
         <button
           className="logout-button"
           onClick={() => {
-            setAuthentication(false);
             history.push("/login");
           }}
         >
