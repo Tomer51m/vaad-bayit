585476de1485f626438e2f9614e0d39a1a2e4e79
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-02-26 09:19:10 +0200

    added logout action

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -62,81 +62,31 @@ export function signup(user) {
   };
 }
 
-export function createMasterUser(user) {
-  console.log("create master user", user);
-  return async dispatch => {
-    const response = await fetch(`http://localhost:8080/api/signup/`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({
-        first_name: user.firstName,
-        last_name: user.lastName,
-        email: user.email,
-        password: user.password,
-        passwordConfirmation: user.passwordConfirmation
-      })
+export function logout() {
+  console.log("logout action")
+  return dispatch => {
+    dispatch({
+      type: "LOGOUT"
     });
-    const newUser = await response.json();
-    // dispatch({
-    //   type: "CREATE_USER",
-    //   newUser
-    // });
+    history.push("/login");
   };
 }
 
-export function getUsers() {
-  return async dispatch => {
-    const response = await fetch("http://localhost:8080/api/users/");
-    const users = await response.json();
-    dispatch({
-      type: "GET_USERS",
-      users
-    });
-  };
-}
-
-export function updateUser(resident, res_id) {
-  return async dispatch => {
-    const response = await fetch(`http://localhost:8080/api/users/`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({
-        res_id: res_id,
-        first_name: resident.firstName,
-        last_name: resident.lastName,
-        apartment_number: resident.apartmentNumber,
-        floor_number: resident.floorNumber,
-        is_owner: resident.isOwner
-      })
-    });
-    const updatedUser = await response.json();
-    console.log("1121212", updatedUser);
-    dispatch({
-      type: "UPDATE_USER",
-      updatedUser
-    });
-  };
-}
-
-export function deleteUser(res_id) {
-  return async dispatch => {
-    const response = await fetch("http://localhost:8080/api/users/", {
-      method: "DELETE",
-      headers: {
-        "Content-type": "application/json"
-      },
-      body: JSON.stringify({
-        res_id: res_id
-      })
-    });
-    await response.json();
-    dispatch({
-      type: "DELETE_USER",
-      res_id
-    });
-  };
-}
+// export function deleteUser(res_id) {
+//   return async dispatch => {
+//     const response = await fetch("http://localhost:8080/api/users/", {
+//       method: "DELETE",
+//       headers: {
+//         "Content-type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         res_id: res_id
+//       })
+//     });
+//     await response.json();
+//     dispatch({
+//       type: "DELETE_USER",
+//       res_id
+//     });
+//   };
+// }
