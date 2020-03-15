eb54b9bff063ca5edbabd522d0acc55f73aa4623
Tomer Matmon <25210484+Tomer51m@users.noreply.github.com>
2020-03-15 20:23:32 +0200

    changed logic from residents to buildings

--- src/components/residentList/ResidentsList.js
+++ src/components/residentList/ResidentsList.js
@@ -2,18 +2,19 @@ import React from "react";
 import "./residentsList.scss";
 import { Link } from "react-router-dom";
 
-function ResidentsList({ users, match, location }) {
-  if (!users) {
+function ResidentsList({ buildings, users, match, location }) {
+  console.log("list", buildings)
+  if (!buildings) {
     return <div>loading...</div>;
   } else {
     return (
       <div className="residentList-container">
         <ul className="residentList-list">
-          {users.map(user => {
+          {buildings.map(building => {
             return (
-              <li className="residentList-li" key={user.res_id}>
-                <Link to={`/home/residents/${user.res_id}`} className="residentLink">
-                  {user.first_name} {user.last_name}
+              <li className="residentList-li" key={building.building_id}>
+                <Link to={`/home/residents/${building.building_id}`} className="residentLink">
+                  {building.city} {building.street} {building.building_number}
                 </Link>
               </li>
             );
